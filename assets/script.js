d4eb2a21d4f54d57f72f766579e0fe46a0ef679d
aleganza <ganzarolli.alessio@gmail.com>
2022-07-10 04:27:01 +0200

    modal gallery

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
-let banner = $("#banner");
+// banner
 
+let banner = $("#banner");
 
 $(window).scroll(function () {
     let scroll = $(window).scrollTop();
     console.log(scroll);
 
     banner.css("top", scroll*0.8 + "px");
+});
+
+// modal image gallery
+
+let open = $("#open-modal");
+
+open.click(function () {
+    open.parent().find(".modal-gallery").css("display", "block");
+});
+
+let close = $("#close-modal");
+
+close.click(function () {
+    close.parent().css("display", "none");
 });
