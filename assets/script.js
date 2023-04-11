4d6427a96a399c98a3a59ca18fb7e16298ba0cbf
aleganza <ganzarolli.alessio@gmail.com>
2023-04-11 03:23:37 +0200

    javscript function errors (not fixed)

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
+// api
+
+async function fetchAPI() {
+    /* const url = "https://minecraft.curseforge.com/api/game/versions"; */
+    const url = "https://my-burger-api.herokuapp.com/burgers";
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin:": "*",
+            "X-Api-Token": "0a330a6c-11ec-4ea3-898f-8de95fa63080"
+        }
+    }).catch(err => {
+        console.log(err.response.data);
+    });
+    const data = await response.json();
+
+    console.log(data);
+}
+
 // banner
 
 let banner = $("#banner");
