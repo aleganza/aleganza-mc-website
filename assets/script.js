7cc8e40b58c22f69d74646aa0e12bcccc96aa2f7
aleganza <ganzarolli.alessio@gmail.com>
2023-07-19 00:47:56 +0200

    fix that no main sections were shown

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ let nav_textures =  $("#nav-textures")
 let main_mods = $("#main-mods")
 let main_textures = $("#main-textures")
 
+// when a nav element is clicked, its corrispondent main section is shown and the nav element set to active
 nav_textures.click(() => { 
     nav_textures.addClass("active")
     nav_main.removeClass("active")
@@ -19,3 +20,17 @@ nav_main.click(() => {
     main_textures.css("display", "none")
     main_mods.css("display", "block")
 });
+
+// fix that when going from mobile to other screens, no main sections were shown
+$(window).resize(() => {
+    if($(window).width() <= 850) {
+        main_textures.css("display", "none")
+        main_mods.css("display", "none")
+    } else {
+        if(nav_main.hasClass("active")) {
+            main_mods.css("display", "block")
+        } else if(nav_textures.hasClass("active")) {
+            main_textures.css("display", "block")
+        }
+    }
+})
