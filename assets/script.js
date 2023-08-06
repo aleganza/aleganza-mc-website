58d791647737251f63b0a63548e5b90b01301d80
aleganza <ganzarolli.alessio@gmail.com>
2023-08-07 01:17:56 +0200

    nav and page changes
    mods and textures are in the same page
    add itch.io page
    link are now in the footer

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,36 @@
-let nav_main =  $("#nav-mods")
-let nav_textures =  $("#nav-textures")
+let nav_main =  $("#nav-a")
+let nav_b =  $("#nav-b")
 
-let main_mods = $("#main-mods")
-let main_textures = $("#main-textures")
+let main_a = $("#main-a")
+let main_b = $("#main-b")
 
 // when a nav element is clicked, its corrispondent main section is shown and the nav element set to active
-nav_textures.click(() => { 
-    nav_textures.addClass("active")
+nav_b.click(() => { 
+    nav_b.addClass("active")
     nav_main.removeClass("active")
 
-    main_mods.css("display", "none")
-    main_textures.css("display", "block")
+    main_a.css("display", "none")
+    main_b.css("display", "block")
 });
 
 nav_main.click(() => { 
     nav_main.addClass("active")
-    nav_textures.removeClass("active")
+    nav_b.removeClass("active")
 
-    main_textures.css("display", "none")
-    main_mods.css("display", "block")
+    main_b.css("display", "none")
+    main_a.css("display", "block")
 });
 
 // fix that when going from mobile to other screens, no main sections were shown
 $(window).resize(() => {
     if($(window).width() <= 850) {
-        main_textures.css("display", "none")
-        main_mods.css("display", "none")
+        main_b.css("display", "none")
+        main_a.css("display", "none")
     } else {
         if(nav_main.hasClass("active")) {
-            main_mods.css("display", "block")
-        } else if(nav_textures.hasClass("active")) {
-            main_textures.css("display", "block")
+            main_a.css("display", "block")
+        } else if(nav_b.hasClass("active")) {
+            main_b.css("display", "block")
         }
     }
 })
